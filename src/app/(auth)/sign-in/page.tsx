73da09ec4b43cf74e079c3896ca8b6f5b847c3a4
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2025-04-20 16:06:57 +0530

    implement sign-up page with username uniqueness check, form validation, and submission handling

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,65 +1,8 @@
-'use client'
-
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import Link from "next/link"
-import { useEffect, useState } from "react"
-import { useDebounceValue } from 'usehooks-ts'
-import { toast } from "sonner"
-import { useRouter } from "next/navigation"
-import { signUpSchema } from "@/schemas/SignUpSchema"
-import axios, {AxiosError} from 'axios'
-import { ApiResponse } from "@/types/ApiResponse"
-
+import React from 'react'
 
 const page = () => {
-  const [username, setUsername] = useState('')
-  const [usernameMessage, setUsernameMessage] = useState('')
-  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
-  const [isSubmitting, setIsSubmitting] = useState(false)
-
-  const debouncedUsername = useDebounceValue(username, 300)
-  const router = useRouter();
-
-  //zod implementation
-  const form = useForm<z.infer<typeof signUpSchema>>({
-    resolver: zodResolver(signUpSchema),
-    defaultValues: {
-      username: '',
-      email: '',
-      password: '',
-    }
-  })
-
-  useEffect(() => {
-    const checkUsernameUnique = async () => {
-      if(debouncedUsername){
-        setIsCheckingUsername(true)
-        setUsernameMessage('')
-        try {
-          const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`)
-         setUsernameMessage(response.data.message)
-        
-        } catch (error) {
-          const AxiosError = error as AxiosError<ApiResponse>
-          setUsernameMessage(
-            AxiosError.response?.data.message ?? "Error checking username"
-          )
-        }finally{
-          setIsCheckingUsername(false)
-        }
-      }
-    }
-    checkUsernameUnique()
-  },[debouncedUsername])
-
-  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
-      
-  }
-
   return (
-    <div>page</div>
+    <div>sign-in page</div>
   )
 }
 
